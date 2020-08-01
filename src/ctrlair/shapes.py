87c20c6f2a9cec9886a490a4b25ef61478fd79b2
João Palmeiro <joaommpalmeiro@gmail.com>
2020-08-01 13:19:14 +0100

    Added: Shield, up-pointing angle bracket, and diamond shapes.

--- src/ctrlair/shapes.py
+++ src/ctrlair/shapes.py
@@ -6,6 +6,7 @@ square = Shape("Square", "M -1 1 L 1 1 L 1 -1 L -1 -1 Z")
 circle = Shape(
     "Circle", "M -1 0 A 1 1 0 0 0 0 1 A 1 1 0 0 0 1 0 A 1 1 0 0 0 0 -1 A 1 1 0 0 0 -1 0"
 )
+diamond = Shape("Diamond", "M 0 -1 L 1 0 L 0 1 L -1 0 Z")
 
 up_pointing_triangle = Shape("Up-Pointing Triangle", "M 0 -1 L -1 1 L 1 1 Z")
 down_pointing_triangle = Shape("Down-Pointing Triangle", "M 0 1 L -1 -1 L 1 -1 Z")
@@ -17,7 +18,7 @@ star = Shape(
     "M 0 0.6 L 0.6 0.9 L 0.5 0.2 L 1 -0.2 L 0.3 -0.3 L 0 -0.9 L -0.3 -0.3 L -1 -0.2 L -0.5 0.2 L -0.6 0.9 L 0 0.6 Z",
 )
 
-line_cross = Shape("Line Cross", "M 1 1 L -1 -1 M -1 1 L 1 -1")
+times = Shape("Times", "M 1 1 L -1 -1 M -1 1 L 1 -1")
 
 lower_right_triangle = Shape("Lower Right Triangle", "M -1 1 L 1 -1 L 1 1 Z")
 lower_left_triangle = Shape("Lower Left Triangle", "M 1 1 L -1 -1 L -1 1 Z")
@@ -35,4 +36,10 @@ up_pointing_squared_petal = Shape(
 )
 down_pointing_squared_petal = Shape(
     "Down-Pointing Squared Petal", "M 0 1 L 1 0 A 1 1 0 0 0 -1 0 L -1 0 Z"
+)
+
+shield = Shape("Shield", "M -1 -1 L -1 0 A 1 1 0 0 0 1 0 L 1 -1 Z")
+
+up_pointing_angle_bracket = Shape(
+    "Up-Pointing Angle Bracket", "M 0 -1 L 1 0 L 1 1 L 0 0 L -1 1 L -1 0 Z"
 )

7b724ba58e2f22e2c87bd1e53118167e39fad940
João Palmeiro <joaommpalmeiro@gmail.com>
2020-08-01 02:14:39 +0100

    Added: Triangle shapes.

--- src/ctrlair/shapes.py
+++ src/ctrlair/shapes.py
@@ -3,8 +3,14 @@ from typing import NamedTuple
 Shape = NamedTuple("Shape", [("Name", str), ("Path", str)])
 
 square = Shape("Square", "M -1 1 L 1 1 L 1 -1 L -1 -1 Z")
+circle = Shape(
+    "Circle", "M -1 0 A 1 1 0 0 0 0 1 A 1 1 0 0 0 1 0 A 1 1 0 0 0 0 -1 A 1 1 0 0 0 -1 0"
+)
 
 up_pointing_triangle = Shape("Up-Pointing Triangle", "M 0 -1 L -1 1 L 1 1 Z")
+down_pointing_triangle = Shape("Down-Pointing Triangle", "M 0 1 L -1 -1 L 1 -1 Z")
+left_pointing_triangle = Shape("Left-Pointing Triangle", "M -1 0 L 1 1 L 1 -1 Z")
+right_pointing_triangle = Shape("Right-Pointing Triangle", "M 1 0 L -1 1 L -1 -1 Z")
 
 star = Shape(
     "Star",
@@ -13,4 +19,7 @@ star = Shape(
 
 line_cross = Shape("Line Cross", "M 1 1 L -1 -1 M -1 1 L 1 -1")
 
+lower_right_triangle = Shape("Lower Right Triangle", "M -1 1 L 1 -1 L 1 1 Z")
 lower_left_triangle = Shape("Lower Left Triangle", "M 1 1 L -1 -1 L -1 1 Z")
+upper_right_triangle = Shape("Upper Right Triangle", "M 1 1 L -1 -1 L 1 -1 Z")
+upper_left_triangle = Shape("Upper Left Triangle", "M -1 1 L -1 -1 L 1 -1 Z")

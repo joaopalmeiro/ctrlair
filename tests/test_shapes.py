996f3de3ee30b40ecdce18ee608f67cd246fb8b0
João Palmeiro <joaommpalmeiro@gmail.com>
2020-08-02 01:07:06 +0100

    Updated: E741 and max-line-length.

--- tests/test_shapes.py
+++ tests/test_shapes.py
@@ -7,13 +7,13 @@ VALID_SVG_PATH_COMMANDS = ["M", "L", "H", "V", "C", "S", "Q", "T", "A", "Z"]
 def svg_path_string_within_square_bounding_box(path: str) -> bool:
     numbers = [int(p) for p in path.split() if p.lstrip("-").isdigit()]
 
-    return all(n >= -1 and n <= 1 for n in numbers)
+    return all(num >= -1 and num <= 1 for num in numbers)
 
 
 def svg_path_string_commands(path: str) -> bool:
     letters = [p for p in path.split() if not p.lstrip("-").isdigit()]
 
-    return all(l in VALID_SVG_PATH_COMMANDS for l in letters)
+    return all(letter in VALID_SVG_PATH_COMMANDS for letter in letters)
 
 
 class TestShapes(unittest.TestCase):
